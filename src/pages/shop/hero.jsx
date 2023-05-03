666617f898971a9b2a7388aa328afb64355ed86d
Tim van der Kleijn <timkleijn@gmail.com>
2023-05-03 19:28:57 +0200

    Updated products/updated hero

--- src/pages/shop/hero.jsx
+++ src/pages/shop/hero.jsx
@@ -6,9 +6,7 @@ export const Hero = () => {
     <div className='relative h-screen mb-20 -mt-20 p-4'>
         <img className='absolute inset-0 object-cover h-full max-h-screen w-screen -z-10 brightness-50' src={neutraal} />
         <div className='max-w-7xl mx-auto flex flex-col justify-center items-center w-full h-full'>
-          <h1 className='text-3xl md:text-4xl lg:text-6xl text-white font-extrabold opacity-100 mb-5 text-center'>Waarom kiezen voor de Buurtwinkel?</h1>
-          <h2 className='text-white text-xl lg:text-2xl font-bold mb-5 text-center'>Bestel nu bij de Buurtwinkel!
-          </h2>
+          <h1 className='text-3xl md:text-4xl lg:text-6xl text-white font-extrabold opacity-100 mb-5 text-center'>Bestel nu bij de Buurtwinkel!</h1>
           <button className='rounded-full bg-primary text-white font-bold px-4 py-2 w-fit text-xl hover:bg-primaryhover' onClick={e => {
             let shop = document.getElementById("shop-scroll-anchor");
             e.preventDefault();
